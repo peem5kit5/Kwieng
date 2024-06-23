491c9a902252508406121e7e250b643bc26c6a63
MSI\Windows 11 <peem5kit5@hotmail.com>
2024-06-23 15:07:47 +0700

    - change to fire base

--- Kwieng/Assets/Script/GoogleHandler/LoginController.cs
+++ Kwieng/Assets/Script/GoogleHandler/LoginController.cs
@@ -1,23 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GooglePlayGames;
-using GooglePlayGames.BasicApi;
 
 public class LoginController : MonoBehaviour
 {
-    public void GoogleLogin() => PlayGamesPlatform.Instance.Authenticate(Processing);
-    internal void Processing(SignInStatus _status)
+    public void GoogleLogin()
     {
-        if (_status == SignInStatus.Success)
-        {
 
-        }
-        else
-        {
-            GuestLogin();
-            return;
-        }  
     }
 
     public void GuestLogin()
